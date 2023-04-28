9fc6837dc036d71499df7712a235e22c89e9f5dd
DelfinaGist <fullstackcodez@gmail.com>
2023-04-28 10:40:12 -0500

    commit

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const PORT = proess.env.PORT || 3000;
+const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send(
-     `<h1>99 Bottles of beer on the wall</h1>   
-     <a href ='/98'>Take one down and pass it around</a>`
-     );
+app.get('/', (req, res) => {
+  res.send(`
+    <h1> 99 Bottles of beer on the wall</h1>
+    <a href="/98">Take one down, pass it around</a>
+  `);
 });
 
-app.get("/:number_of_bottles", (req, res) => {
-    const numberOfBottles = req.params.number_of_bottles;
-    if (numberOfBottles > 0) {
-        res.send(
-
-        )
-    }
-})
-
+app.get('/:number_of_bottles', (req, res) => {
+  const num = req.params.number_of_bottles;
+  res.send(`
+    <h1>${num} Bottles of beer of the wall</h1>
+    <a href="/${num <= 0 ? '' : num - 1}">${
+    num <= 0 ? 'Start Over' : 'Take one down, pass it around'
+  }</a>
+  `);
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+  console.log(`Listening on port: ${PORT}`);
 });
